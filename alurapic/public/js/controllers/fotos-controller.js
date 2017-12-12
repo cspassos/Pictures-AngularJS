bcb9ab960d6978a0b8201198e24fe74dcde34d89
cspassos <caic.souza@hotmail.com>
2017-12-12 14:34:11 -0200

    Construindo o alicerce da aplicação

---
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+//$scope = para o controler conseguir adicionar dados em minha view  
+    $scope.foto = {
+        titulo: 'Leão',
+        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
+}); 
