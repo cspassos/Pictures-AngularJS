4e7d510d3151737d97678b8ab6755cb62fa449c2
cspassos <caic.souza@hotmail.com>
2017-12-13 13:11:27 -0200

    Dividir para conquistar!

--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -1,1 +1,18 @@
-angular.module('alurapic', ['minhasDiretivas','ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas','ngAnimate', 'ngRoute'])
+.config(function($routeProvider, $locationProvider){
+
+    $locationProvider.html5Mode(true);
+    $routeProvider.when('/fotos', {
+         //Quando a url for http://localhost:3000/#/fotos executa o 
+         // partials/principal.html
+        templateUrl: 'partials/principal.html',
+        //E executa o controler 'FotosController'
+        controller: 'FotosController'
+    });
+
+    $routeProvider.when('/fotos/new', {
+        templateUrl: 'partials/foto.html'
+    });
+    //Caso Contrario, caso ele digite uma url invalida ele leva para /fotos 
+    $routeProvider.otherwise({ redirectTo: '/fotos'});
+});
