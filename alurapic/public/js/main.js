215b91e2e7a717f99c31061fee280e6dd7752748
cspassos <caic.souza@hotmail.com>
2017-12-15 11:07:00 -0200

    Criando tela de cadastrar e validacao

--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -2,6 +2,7 @@ angular.module('alurapic', ['minhasDiretivas','ngAnimate', 'ngRoute'])
 .config(function($routeProvider, $locationProvider){
 
     $locationProvider.html5Mode(true);
+    
     $routeProvider.when('/fotos', {
          //Quando a url for http://localhost:3000/#/fotos executa o 
          // partials/principal.html
@@ -11,7 +12,8 @@ angular.module('alurapic', ['minhasDiretivas','ngAnimate', 'ngRoute'])
     });
 
     $routeProvider.when('/fotos/new', {
-        templateUrl: 'partials/foto.html'
+        templateUrl: 'partials/foto.html',
+        controller: 'FotoController'
     });
     //Caso Contrario, caso ele digite uma url invalida ele leva para /fotos 
     $routeProvider.otherwise({ redirectTo: '/fotos'});
